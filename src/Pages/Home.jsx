5de8b8cfe2b39df46342d94f23bbf68284607060
Ricardo <rbrumjunio@gmail.com>
2024-01-25 17:38:00 -0300

    adicionando a base da pagina home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,20 @@
 import { Footer, NavBar } from "../Components";
+import { homeImage } from '../images';
 
 export default function Home() {
   return (
     <div>
       <NavBar/>
+      <main>
+        <div className="d-flex flex-row" home>
+          <div className="container">
+            <h3 className="text-uppercase">Olá</h3>
+            <h1 className="text-uppercase">Eu sou o Ricardo Pereira</h1>
+            <h5 className="text-uppercase">Desenvolvedor Junior FullStack</h5>
+          </div>
+          <img src={homeImage} alt="homeigm" width='300'/>
+        </div>
+      </main>
       <Footer/>
     </div>
   );
---
+++ src/images/index.js
@@ -0,0 +1,1 @@
+export { default as homeImage } from "./HomeImg.png";
