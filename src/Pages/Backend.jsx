f073370a5fda40d456b056b3b87d68d40626421e
Ricardo <rbrumjunio@gmail.com>
2024-02-23 07:55:17 -0300

    Back end data adicionado e implementado a pagina backend

--- src/Pages/Backend.jsx
+++ src/Pages/Backend.jsx
@@ -1,10 +1,16 @@
-import { Footer, NavBar } from "../Components";
+import { Footer, NavBar, ProjectsMold } from "../Components";
+import backEndData from "../helpers/BackEndData.js";
 
 export default function Backend() {
   return (
     <div className="home">
       <NavBar/>
       <h1>Backend</h1>
+      <div>
+        {
+          ProjectsMold(backEndData)
+        }
+      </div>
       <Footer/>
     </div>
   );
---
+++ src/helpers/BackEndData.js
@@ -0,0 +1,36 @@
+const backEndData = [
+  {
+    lngName: "Typescript",
+    projects: [
+      {
+        name: "Trybe Futebol Clube",
+        url: "https://github.com/RicardoPBJ/Projeto_Trybe_Futebol_Clube",
+      },
+      {
+        name: "Tryber and Dragons",
+        url: "https://github.com/RicardoPBJ/trybers-and-dragons",
+      },
+      { name: "Blogs Api", url: "https://github.com/RicardoPBJ/blogs-api" },
+    ],
+  },
+  {
+    lngName: "C#",
+    projects: [
+      { name: "Trybets", url: "https://github.com/RicardoPBJ/trybets" },
+      { name: "Trybe Hotel", url: "https://github.com/RicardoPBJ/trybe_hotel" },
+      { name: "Trybe Games", url: "https://github.com/RicardoPBJ/trybe-games" },
+    ],
+  },
+  {
+    lngName: "Python",
+    projects: [
+      {
+        name: "Restaurant Orders",
+        url: "https://github.com/RicardoPBJ/Projeto_Restaurant_orders",
+      },
+      { name: "TING", url: "https://github.com/RicardoPBJ/ting" },
+      { name: "Algoritimos", url: "https://github.com/RicardoPBJ/algoritimos" },
+    ],
+  },
+];
+export default backEndData;
