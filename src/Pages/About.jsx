070c035ba4ee472327eb22fa831891a0682fc3bc
Ricardo <rbrumjunio@gmail.com>
2024-02-23 03:36:57 -0300

    Adicionando o helper text e implementando a pagina About

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,22 @@
 import { Footer, NavBar } from "../Components";
+import { Texts } from "../helpers";
 
 export default function About() {
+  const { about } = Texts
   return (
-    <div className="home">
+    <div className="about">
       <NavBar/>
-      <h1>About</h1>
+      <main>
+        <section className="d-flex flex-row">
+          <img src='' alt='aboutImage' />
+          <h1>Vamos a uma introdução sobre mim</h1>
+          <div>
+            <p>{about.text1}</p>
+            <p>{about.text2}</p>
+            <p>{about.text3}</p>
+          </div>
+        </section>
+      </main>
       <Footer/>
     </div>
   );
