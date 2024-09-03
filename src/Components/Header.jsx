4d34903843fc6576abe65af3ba2b41cd7d27f2f4
RicardoPBJ <rbrumjunio@gmail.com>
2024-09-03 19:36:36 -0300

    aplicando logo ao header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,14 @@
 import "../Styles/Header.css"
 import { NavBar } from "./";
+import { logo } from "../images";
 
-function Header({title}) {
+function Header() {
   return (
     <div className="header-container">
+      <div className="header-logo">
+        <img src={logo} alt="logo" />
+        <span>Ricardo Pereira</span>
+      </div>
       <NavBar />
     </div>
   );
