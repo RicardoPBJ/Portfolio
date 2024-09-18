51247701741190d546cb51ea20f82f424cdbab28
RicardoPBJ <rbrumjunio@gmail.com>
2024-09-18 13:05:57 -0300

    ajustando estilos de about e techicons

--- src/Components/TechIcons.jsx
+++ src/Components/TechIcons.jsx
@@ -4,14 +4,17 @@ import { IconRender, TechIconsArr } from "../helpers";
 function TechIcons() {
   return (
     <main className="icons-list">
-      {TechIconsArr.map(({ icon, name }) => {
-        return (
-          <div className="icon-el">
-            <IconRender icon={icon} size={50} />
-            <div>{name}</div>
-          </div>
-        );
-      })}
+      <span>Principais Tecnologia:</span>
+      <div className="icons-container">
+        {TechIconsArr.map(({ icon, name }) => {
+          return (
+            <div className="icon-el">
+              <IconRender icon={icon} size={50} />
+              <div>{name}</div>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
